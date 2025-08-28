5435907aa4f32e348425548fc68511dbb56c715c
Lynda Kodilinye-Nwogem <121471419+Lynnhaute@users.noreply.github.com>
2025-08-28 12:20:04 +0100

    Add files via upload

---
+++ conditional statement.py
@@ -0,0 +1,14 @@
+ # if/elif/else statement for a college with a grading system
+
+grade = int(input("enter your grade: "))
+
+if grade >= 98:
+    print("A")
+elif grade >= 80:
+    print("B")     
+elif grade >= 70:
+    print("C")   
+elif grade >= 68:
+     print("D")
+else:
+    print("F")   
